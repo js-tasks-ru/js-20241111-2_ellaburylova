254b21c518cd12582c34591b34e212e7313d1fd5
EllaBurylova <e.burylova@simplecompany.io>
2024-11-19 10:09:33 +0300

    Task 3.1, 3.2, 3.3, 3.4

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size <= 0) return "";
+  if (size === undefined) return string;
 
+  const resArray = [string[0]];
+  let counter = 1;
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[i - 1]) {
+      if (counter < size) {
+        resArray.push(string[i]);
+        counter += 1;
+      }
+    } else {
+      resArray.push(string[i]);
+      counter = 1;
+    }
+  }
+  return resArray.join("");
 }
