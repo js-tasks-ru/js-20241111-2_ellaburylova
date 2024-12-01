b2c292e59a6f892a1d853e5a54e6b6f7bbad3bf3
EllaBurylova <e.burylova@simplecompany.io>
2024-12-01 18:36:38 +0300

    5.1 notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  static lastNotification;
+  element;
+  constructor(message = "", { duration = 0, type = "success" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement(this.createTemplate());
+  }
 
+  createTemplate() {
+    return `
+    <div class="notification ${
+      this.type
+    }" style="--value:${this.getTimerValue()}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+  `;
+  }
+
+  createElement(template) {
+    const element = document.createElement("div");
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  show(targetElement = document.body) {
+    if (NotificationMessage.lastNotification) {
+      NotificationMessage.lastNotification.destroy();
+    }
+    NotificationMessage.lastNotification = this;
+
+    targetElement.appendChild(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  getTimerValue() {
+    return this.duration / 1000;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
